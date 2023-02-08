dca099c77962cb903b25342792debc2e84e2e913
Leandro Bolivar <leandrob131@gmail.com>
2023-02-08 09:11:16 -0500

    Moved teensy define statements to teensy.h

--- Chord-Organ/Chord-Organ.ino
+++ Chord-Organ/Chord-Organ.ino
@@ -9,33 +9,11 @@
 
 #include "Settings.h"
 #include "Waves.h"
+#include "teensy.h"
 
 // #define DEBUG_STARTUP
 // #define DEBUG_MODE
 // #define CHECK_CPU
-
-#define CHORD_POT_PIN 9 // pin for Chord pot
-#define CHORD_CV_PIN 6 // pin for Chord CV 
-#define ROOT_POT_PIN 7 // pin for Root Note pot
-#define ROOT_CV_PIN 8 // pin for Root Note CV
-#define RESET_BUTTON 8 // Reset button 
-#define RESET_LED 11 // Reset LED indicator 
-#define RESET_CV 9 // Reset pulse in / out
-#define BANK_BUTTON 2 // Bank Button 
-#define LED0 6
-#define LED1 5
-#define LED2 4
-#define LED3 3
-
-// REBOOT CODES 
-#define RESTART_ADDR       0xE000ED0C
-#define READ_RESTART()     (*(volatile uint32_t *)RESTART_ADDR)
-#define WRITE_RESTART(val) ((*(volatile uint32_t *)RESTART_ADDR) = (val))
-
-#define ADC_REF 3.3
-#define ADC_BITS 13
-#define ADC_MAX_VAL 8192
-#define CHANGE_TOLERANCE 64
 
 #define SINECOUNT 8
 #define LOW_NOTE 36
@@ -248,7 +226,7 @@ void setup(){
         // but they are not enabled in the config then change back to sine
         waveform = 0;
     }
-
+    
     glide = settings.glide;
     glideTime = settings.glideTime;
     oneOverGlideTime = 1.0 / (float) glideTime;
@@ -286,14 +264,6 @@ void setup(){
         flashingWave = true;
         waveformIndicatorTimer = 0;
     }
-
-#ifdef DEBUG_STARTUP
-    Serial.print("Root Clamp Low ");
-    Serial.println(rootClampLow);
-    Serial.print("Root Map Coeff ");
-    Serial.println(rootMapCoeff * 100);
-#endif
-
 }
 
 boolean openSDCard() {
---
+++ Chord-Organ/teensy.h
@@ -0,0 +1,22 @@
+#define CHORD_POT_PIN 9 // pin for Chord pot
+#define CHORD_CV_PIN 6 // pin for Chord CV 
+#define ROOT_POT_PIN 7 // pin for Root Note pot
+#define ROOT_CV_PIN 8 // pin for Root Note CV
+#define RESET_BUTTON 8 // Reset button 
+#define RESET_LED 11 // Reset LED indicator 
+#define RESET_CV 9 // Reset pulse in / out
+#define BANK_BUTTON 2 // Bank Button 
+#define LED0 6
+#define LED1 5
+#define LED2 4
+#define LED3 3
+
+// REBOOT CODES 
+#define RESTART_ADDR       0xE000ED0C
+#define READ_RESTART()     (*(volatile uint32_t *)RESTART_ADDR)
+#define WRITE_RESTART(val) ((*(volatile uint32_t *)RESTART_ADDR) = (val))
+
+#define ADC_REF 3.3
+#define ADC_BITS 13
+#define ADC_MAX_VAL 8192
+#define CHANGE_TOLERANCE 64
